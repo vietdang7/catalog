7ca32dbba73dfdf3e72b2a7d2e2b6319c67ad247
Viet Dang Quoc <thedangs7@gmail.com>
2017-11-22 22:38:17 +0200

    Add routing for '/' and '/quotes'

---
+++ quotes.py
@@ -0,0 +1,30 @@
+#Import flask and sqlalchemy library
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+#Import from database_setup.py
+from database_setup import Base, User, Category, QuoteItem
+
+app = Flask(__name__)
+
+#Create database connection
+engine = create_engine('sqlite:///quotes.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+#Routing for '/' and '/quotes'
+@app.route('/')
+@app.route('/quotes')
+def showQuotes():
+    return 'This page will show quotes'
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
