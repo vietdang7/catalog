d170891b6db00d9d17e5206ff0f274f55e3c3318
Viet Dang Quoc <thedangs7@gmail.com>
2017-11-26 01:06:46 +0200

    Add functions for user

--- catalog.py
+++ catalog.py
@@ -137,6 +137,29 @@ def gconnect():
     return output
 
 
+# Functions for User
+def createUser(login_session):
+    newUser = User(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
+
 #Making API Endpoint for '/items/JSON'
 @app.route('/items/JSON')
 def itemsJSON():
