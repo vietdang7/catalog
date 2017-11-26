465d55ffbd9ab0b63310f8b2da43f9812b9d946b
Viet Dang Quoc <thedangs7@gmail.com>
2017-11-26 18:47:46 +0200

    Update catalog.py

--- catalog.py
+++ catalog.py
@@ -192,6 +192,26 @@ def gdisconnect():
         return response
 
 
+# Disconnect based on provider
+@app.route('/disconnect')
+def disconnect():
+    if 'provider' in login_session:
+        if login_session['provider'] == 'google':
+            gdisconnect()
+            del login_session['gplus_id']
+            del login_session['access_token']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        del login_session['user_id']
+        del login_session['provider']
+        flash("You have successfully been logged out.")
+        return redirect(url_for('showItems'))
+    else:
+        flash("You were not logged in")
+        return redirect(url_for('showItems'))
+
+
 #Making API Endpoint for '/items/JSON'
 @app.route('/items/JSON')
 def itemsJSON():
